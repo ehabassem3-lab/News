fb47f69de8c6e4643b7cc62fb0228e8b5c752e7c
ehabassem3.lab <ehabassem3@gmail.com>
2026-05-01 11:13:32 +0300

    Adding the BottomSheet To THe Seached Items

--- app/src/main/java/com/example/compose_first/screens/news/SearchedItems.kt
+++ app/src/main/java/com/example/compose_first/screens/news/SearchedItems.kt
@@ -1,18 +1,27 @@
 package com.example.compose_first.screens.news
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.compose_first.models.ArticlesItem
 import com.example.compose_first.viewmodels.SearchViewModel
 
 @Composable
@@ -22,6 +31,9 @@ fun SearchedItem(sources : String?){
     var isLoading = viewModel.isLoadingArticle.observeAsState()
     var isError = viewModel.isErrorArticle.observeAsState()
     var Articles = viewModel.ArticleSearched.observeAsState()
+    var selectedArticle = remember {
+        mutableStateOf<ArticlesItem?>(null)
+    }
     DisposableEffect(sources) {
         viewModel.Search(sources?:"")
 
@@ -29,45 +41,62 @@ fun SearchedItem(sources : String?){
 
         }
     }
-    LazyColumn (
-        modifier = Modifier
-            .background(colorScheme.background).
-            fillMaxSize(),
+    Column( modifier = Modifier
+        .background(colorScheme.background).
+        fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally) {
-        item{
-            if (isLoading.value == true){
-                onLoading("The Articles Is Loading ")
+        LazyColumn (
+            modifier = Modifier
+                .background(colorScheme.background).
+                fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            item{
+                if (isLoading.value == true){
+                    onLoading("The Articles Is Loading ")
+                }
+                if (isError?.value?.isNotEmpty() == true && Articles.value?.isEmpty() == null ){
+                    onError("Try Again") {
+                        viewModel.Search(sources?:"")
+
+
+
+
+
+
+
+                    }
+                }
             }
-            if (isError?.value?.isNotEmpty() == true && Articles.value?.isEmpty() == null ){
-                onError("Try Again") {
-                    viewModel.Search(sources?:"")
+            if (Articles.value?.isEmpty() == true ){
+                item {
+                    emptyArticles("No Articles to Show")
 
+                }
 
+            }
 
+            if ( !Articles.value.isNullOrEmpty()){
+                items (Articles.value!!.size){ Article ->
+                    Box(
+                        modifier = Modifier.padding(10.dp).clickable{
+                            selectedArticle.value = Articles.value!!.get(Article)
 
+                        }
+                    ){
+                        NewsSingleITem(Articles.value!![Article])
+                    }
 
-
+                    Spacer(modifier = Modifier.size(10.dp))
 
                 }
             }
-        }
-        if (Articles.value?.isEmpty() == true ){
-            item {
-                emptyArticles("No Articles to Show")
 
-            }
+
 
         }
-
-        if ( !Articles.value.isNullOrEmpty()){
-            items (Articles.value!!.size){ Article ->
-                NewsSingleITem(Articles.value!![Article])
-                Spacer(modifier = Modifier.size(10.dp))
-
-            }
+        if (selectedArticle.value != null){
+            BottomBar(selectedArticle = selectedArticle)
         }
-
-
-
     }
+
 }
